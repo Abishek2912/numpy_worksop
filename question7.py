321ceaea626688b5306fa285d032ac801679f7b2
Abishek2912 <151637548+Abishek2912@users.noreply.github.com>
2024-05-22 11:36:22 +0530

    Update question7.py

--- question7.py
+++ question7.py
@@ -10,3 +10,17 @@ e=[3]
 
 # use append method to do this.
 
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [7, 5, 3, 2, 3]
+d = [2]
+e = [3]
+output = []
+output.append(a)
+output.append(b)
+output.append(c)
+c.append(d)
+d.append(e)
+print(output)
+
+
